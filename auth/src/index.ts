003bd66776d3c39f5b0600f30e56d70d8d17237c
ZenRadar <zenradar@gmail.com>
2021-05-17 20:39:06 +1000

    Add error handler middleware.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,12 +4,15 @@ import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+
 const app = express();
 app.use(json());
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
+app.use(errorHandler);
 
 app.listen(3003, () => {
   console.log('Listening on port 3003.');
