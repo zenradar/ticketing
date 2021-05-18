f8a3a7c40df9c5422b594a92763cfa5683aa1112
ZenRadar <zenradar@gmail.com>
2021-05-18 23:18:28 +1000

    Add mongoose to auth service.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/signin';
@@ -21,7 +23,22 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3003, () => {
-  console.log('Listening on port 3003.');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mogo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3003, () => {
+    console.log('Listening on port 3003.');
+  });
+};
+
+start();
+
+
 
