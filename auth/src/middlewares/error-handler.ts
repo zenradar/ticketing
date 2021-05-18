96b00c1a341a2d4fcaad6fc2d66010bdcacd0a5c
ZenRadar <zenradar@gmail.com>
2021-05-18 20:02:17 +1000

    format errorHandler output.

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -4,11 +4,14 @@ import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof RequestValidationError) {
-        console.log('Handling request validation error');
+        const formattedErrors = err.errors.map(error => {
+            return { message: error.msg, field: error.param };
+        });
+        return res.status(400).send({errors: formattedErrors});
     }
 
     if (err instanceof DatabaseConnectionError) {
-        console.log('Handling database connection error');
+        return res.status(503).send({errors: [{message: err.reason}] });
     }
 
     res.status(400).send( {
