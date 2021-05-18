e64275efc93d70382c51f07c436f5d4b22110fd3
ZenRadar <zenradar@gmail.com>
2021-05-18 19:27:46 +1000

    Refactor database and request validation errors.

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,10 @@
+export class DatabaseConnectionError extends Error {
+
+    reason = 'Error connecting to the database';
+
+    constructor() {
+        super();
+
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+}
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,15 @@
 import {Request, Response, NextFunction} from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log('Something went wrong.', err);
+    if (err instanceof RequestValidationError) {
+        console.log('Handling request validation error');
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        console.log('Handling database connection error');
+    }
 
     res.status(400).send( {
         message: err.message
